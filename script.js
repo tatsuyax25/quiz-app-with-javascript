fbc30c1c6cdd87d08229e92b0f5cc5661b3fa2a4
Miguel Urena <urena_miguel82@outlook.com>
2022-11-18 13:28:38 -0800

    Add code function pt 2

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-    
+    console.log('Started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
@@ -13,3 +17,13 @@ function setNextQuestion() {
 function selectAnswer() {
 
 }
+
+const question = [
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    }
+]

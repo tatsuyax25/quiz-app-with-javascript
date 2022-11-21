6332b34e512336ed254d28a1743fd385786635bb
Miguel Urena <urena_miguel82@outlook.com>
2022-11-21 15:07:13 -0800

    Question function

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+
+const shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
 
 }
 

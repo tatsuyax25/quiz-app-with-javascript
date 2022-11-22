f4f227f29976f4342a78d3374f97ce38824b802b
Miguel Urena <urena_miguel82@outlook.com>
2022-11-22 15:00:55 -0800

    Final commit

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ function showQuestion(question) {
 }
 
 function resetState() {
+    clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)

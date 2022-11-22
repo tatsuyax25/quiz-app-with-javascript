f778f0beeb1eda36e4b1a1f5bddfd76874b327b5
Miguel Urena <urena_miguel82@outlook.com>
2022-11-22 14:45:45 -0800

    Update script.js pt 4

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ function selectAnswer(e) {
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
+    nextButton.classList.remove('hide')
 }
 
 function setStatusClass(element, correct) {
